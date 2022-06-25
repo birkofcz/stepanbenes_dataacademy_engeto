d8c4f8d2db2f6755fb2363947ed303db86f3acf1
Štěpán <stepan.benes@gmail.com>
2022-06-25 11:31:56 +0200

    Main table CREATE query simplifed. SQL queries on main table modified

--- Projekt_SQL_sada.sql
+++ Projekt_SQL_sada.sql
@@ -2,44 +2,59 @@
 -- SQL SADA PRO VÝZKUMNÉ OTÁZKY --
 
 -- 1. rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
-SELECT *,
-CAST(prumerna_mzda_nebo_cena as INT) as mzda_INT 
-FROM t_stepan_benes_project_SQL_primary_final tsbpspf 
-WHERE kod_odvetvi = 'S' -- vyměn si kód odvětví (mzdy)
--- ORDER by mzda ASC -- kontrolní. Nejdou-li roky po sobě, mzdy pokaždé nestoupaly
+SELECT 
+	rok,
+	ROUND(AVG(cena_nebo_mzda)) as prumerna_mzda_nebo_cena,
+	kod_odvetvi,
+	nazev_odvetvi
+FROM t_stepan_benes_project_SQL_primary_final
+WHERE kod_odvetvi  ='A' -- vyměň si kód (mzdy)
+GROUP BY rok 
+-- ORDER by prumerna_mzda_nebo_cena ASC -- kontrolní. Nejdou-li roky po sobě, mzdy pokaždé nestoupaly
 ;
 
 
 -- 2. Kolik je možné koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
 -- mléko 114201 (po litrech)
--- chléb 113301 (po kilech)
--- první a pislední srovnatelní období - 2006 a 2018
+-- chléb 111301 (po kilech)
+-- první a poslední srovnatelní období - 2006 a 2018
 
-
-	SELECT *
+	SELECT 
+		rok,
+		kod_odvetvi,
+		nazev_odvetvi,
+		ROUND(AVG(cena_nebo_mzda)) as prumerna_cena
 	FROM t_stepan_benes_project_SQL_primary_final
-	WHERE kod_odvetvi = '114201'
+	WHERE kod_odvetvi = 114201
+		AND rok = 2006 -- vymen si rok
 	;
 	-- = 14 Kč / litr mléka v roce 2006
 	-- = 20 Kč / litr mléka v roce 2018
 	
-	SELECT *
+	SELECT 
+		rok,
+		kod_odvetvi,
+		nazev_odvetvi,
+		ROUND(AVG(cena_nebo_mzda)) as prumerna_cena
 	FROM t_stepan_benes_project_SQL_primary_final
-	WHERE kod_odvetvi = '111301'
+	WHERE kod_odvetvi = 111301
+		AND rok = 2006 -- vymen si rok
 	;
 	-- = 16 Kč / kilo chleba v roce 2006
 	-- = 24 Kč / kilo chleba v roce 2018
 	
-SELECT *
-FROM t_stepan_benes_project_SQL_primary_final
-WHERE kod_odvetvi = 'S'-- vyměň si kod odvětví (mzdy)
-;
+-- MZDY - dotaz prvni otazky
+
 
 -- 3. Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší meziroční procentuálnbí nárůst)?
-SELECT *,
-CAST(prumerna_mzda_nebo_cena as INT) as cena_INT
-FROM t_stepan_benes_project_SQL_primary_final tsbpspf 
-WHERE kod_odvetvi = '2000001' -- vyměn si kód odvětví (potraviny)
+ SELECT 
+	rok,
+	ROUND(AVG(cena_nebo_mzda)) as prumerna_mzda_nebo_cena,
+	kod_odvetvi,
+	nazev_odvetvi
+FROM t_stepan_benes_project_SQL_primary_final
+WHERE kod_odvetvi  = 111101 -- vyměň si kód (potraviny)
+GROUP BY rok 
 ;
 
 -- 4. Existuje rok, ve kterém byl meziroční nárust cen potravin výrazně vyšší než růst mezd (více než 10 procent?)
@@ -47,9 +62,9 @@ WHERE kod_odvetvi = '2000001' -- vyměn si kód odvětví (potraviny)
 
 -- 5. Má výška HDP vliv na změny ve mzdách a cenách potravin? Neboli, pokud HDP vzroste výrazněji v jednim roce, projeví se to na cenách potravin či mzdách ve stejném nebo následujícím roce výraznějším růstem?
 SELECT *,
-cast (HDP as INT)
+CAST (HDP AS INT)
 FROM t_stepan_benes_project_SQL_secondary_final
-where stat = 'Czech Republic'
+WHERE stat = 'Czech Republic'
 ;
 
 
